c2d97486ec729ec698a9fcc3e843600099c2a484
poissonismyson <md.dovidio@gmail.com>
2024-05-10 17:27:38 +0200

    boh

--- visite su alberi/tentativi.py
+++ visite su alberi/tentativi.py
@@ -1,22 +1,25 @@
-import BinaryTree, BuildTree
+from BinaryTree import *
+
+from BuildTree import *
 
 #Funzione che calcola l'altezza di un albero binario
 
 def trovaAltezza(tree):
     if tree is None:
         return 0
-    if tree.getLEftChild() is None and tree.getRightChild() is None:
+    if tree.getLeftChild() is None and tree.getRightChild() is None:
         return 0
-    h_sin = 1 + trovaAltezza(tree.getLEftChild())
+    h_sin = 1 + trovaAltezza(tree.getLeftChild())
     h_des = 1 + trovaAltezza(tree.getRightChild())
     return max(h_sin, h_des)
 
 #Funzione che verifica se un valore corrisponde all'identificativo di un nodo oppure no
 
 def trovaValore(tree, v):
-    if tree == None:
+    if tree is None:
         return False
-    elif tree.info == v:
+    if tree.getRootVal() == v:
         return True
-    else:
-        return trovaValore(tree.left, v) or trovaValore(tree.right, v)
+    x = trovaValore(tree.getLeftChild(), v)
+    y = trovaValore(tree.getRightChild(), v)
+    return trovaValore(tree.left, v) or trovaValore(tree.right, v)
