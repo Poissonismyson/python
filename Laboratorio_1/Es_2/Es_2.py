9203587fe9a84bdd1e2284567b128b037d9a88c8
mrc <md.dovidio@gmail.com>
2024-05-25 19:41:52 +0200

    modifiche lab 1

--- Laboratorio_1/Es_2/Es_2.py
+++ Laboratorio_1/Es_2/Es_2.py
@@ -1,3 +1,3 @@
- print("Nome \t Data di Nascita")
+print("Nome \t Data di Nascita")
 print("Anna \t 12/04")
 print("Ben \t 28/12")
--- Laboratorio_1/Es_4/Es_4.py
+++ Laboratorio_1/Es_4/Es_4.py
@@ -1,1 +1,5 @@
- 
+ore = int(input("inserire numero di ore: "))
+minuti = int(input("inserire numero di minuti: "))
+secondi = int(input("inserire numero di secondi: "))
+
+print(f"L'equivalente in secondi è pari a {ore*60**2 + minuti*60 + secondi}")
